46e101482e9f8ef81d7ef68abb52c8e64a61aac3
bbaymistery <elgun.ezmemmedov@mail.ru>
2025-06-07 15:27:52 +0400

    portfolio

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import "./styles.css";
 
 const Alert = (props) => {
--- src/components/Portfolio/WorkItems.jsx
+++ src/components/Portfolio/WorkItems.jsx
@@ -5,6 +5,7 @@ const WorkItems = ({ item }) => {
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
       <h3 className="work__title">{item.title}</h3>
+       {/* eslint-disable-next-line react/jsx-no-target-blank */}
       <a target="_blank" href={item.url} className="work__button">
         View
         <i className="bx bx-right-arrow-alt work__button-icon"></i>
